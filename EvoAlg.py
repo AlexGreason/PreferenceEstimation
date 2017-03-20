36bd68402a57cde40e1fc1fe8c847c81509a18ed
AlexGreason <quantum935@yahoo.com>
2017-03-20 14:48:28 -0500

    minor updates to evoalg

--- EvoAlg.py
+++ EvoAlg.py
@@ -4,9 +4,9 @@ import copy.copy as copy
 
 class EvoAlg:
 
-    def __init__(self, popsize, initialpop, mutationparams):
+    def __init__(self, popsize, pop, mutationparams):
         self.popsize = popsize
-        self.initialpop = initialpop
+        self.pop = pop
         self.mutationparams = mutationparams
         """Mutation parameters:
         0: mutation probability (creature)
@@ -25,6 +25,10 @@ class EvoAlg:
             if rand.random < self.mutationparams[3]:
                 creature[i] = rand.normal(0, self.mutationparams[4])
 
+    def random(self, creature):
+        for i in range(len(creature)):
+            creature[i] = rand.normal(0, self.mutationparams[4])
+
     def crossover(self, creature1, creature2):
         swap = rand.randint(0, len(creature1))
         swapindicies = rand.choice(range(len(creature1)), size=swap, replace=False)
@@ -34,5 +38,16 @@ class EvoAlg:
         return result
 
 
-    def generation(self, fitnesses):
-        pass
+    def breed(self, fitnesses):
+        creatures = list(zip(self.pop, fitnesses))
+        creatures.sort(key=lambda x: x[1])
+        select = int(len(self.pop)/self.mutationparams[5])
+        expfit = np.array([np.exp(x[1]) for x in creatures])
+        normfit = expfit/(np.sum(expfit) + 0.00001)
+        selected = np.random.choice(creatures, size=select, replace=False, p=normfit)
+        print([x[1] for x in selected])
+
+if __name__ == "__main__":
+    creatures = [[x] for x in range(10)]
+    fitnesses = [x + .5 for x in range(10)]
+    evoalg = EvoAlg(len(creatures), creatures, [])
